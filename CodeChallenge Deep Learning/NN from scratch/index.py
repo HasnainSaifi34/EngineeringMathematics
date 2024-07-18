26f53e81789f77923a2e97c137c9c099e410cd74
HasnainSaifi34 <hasnainsaifi5670@gmail.com>
2024-07-18 22:54:49 +0530

    trained Neural network to recognise indivisual image from the training dataset:

--- CodeChallenge Deep Learning/NN from scratch/index.py
+++ CodeChallenge Deep Learning/NN from scratch/index.py
@@ -1,5 +1,6 @@
 import numpy as np;
-class NeuralNetwork:
+import pickle
+class NeuralNetwork: 
     def __init__(self, input, hidden_layer, activation_hidden, output_layer, ideal_values, activation_output, learning_rate):
         self.input = np.array(input).reshape(1, -1)  # Ensure input is a row vector
         self.hidden_layer = hidden_layer
@@ -11,7 +12,7 @@ class NeuralNetwork:
         self.ideal_values = np.array(ideal_values).reshape(1, -1)
         self.learning_rate = learning_rate
         self.no_of_neurons = [self.input.shape[1]] + hidden_layer + [output_layer]
-
+ 
         self.W = [np.random.randn(self.no_of_neurons[i + 1], self.no_of_neurons[i]) for i in range(len(self.no_of_neurons) - 1)]
         self.b = [np.random.randn(self.no_of_neurons[i + 1]) for i in range(len(self.no_of_neurons) - 1)]
         self.A = [self.input]
@@ -68,9 +69,18 @@ class NeuralNetwork:
             loss_values.append(loss)
             self.backward_pass()
         return loss_values
-      
-   
-   
-   
-   
-      
+    
+    def save_model(self, file_path):
+        model_params = {
+            'W': self.W,
+            'b': self.b
+        }
+        with open(file_path, 'wb') as f:
+            pickle.dump(model_params, f)
+
+    def load_model(self, file_path):
+        with open(file_path, 'rb') as f:
+            model_params = pickle.load(f)
+        
+        self.W = model_params['W']
+        self.b = model_params['b']    
