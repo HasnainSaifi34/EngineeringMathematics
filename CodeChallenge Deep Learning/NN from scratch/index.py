a6b168584aa84bbcb06a68ba7378cdee51c8c2af
HasnainSaifi34 <hasnainsaifi5670@gmail.com>
2024-07-19 19:36:01 +0530

    trained the network on training set , saved and loaded the network

--- CodeChallenge Deep Learning/NN from scratch/index.py
+++ CodeChallenge Deep Learning/NN from scratch/index.py
@@ -1,15 +1,16 @@
-import numpy as np;
+import numpy as np
 import pickle
-class NeuralNetwork: 
+
+class NeuralNetwork:
     def __init__(self, input, hidden_layer, activation_hidden, output_layer, ideal_values, activation_output, learning_rate):
-        self.input = np.array(input).reshape(1, -1)  # Ensure input is a row vector
+        self.input = np.array(input)  # Ensure input is a 2D array
         self.hidden_layer = hidden_layer
         self.activation_hidden = activation_hidden[0]
         self.activation_hidden_derivative = activation_hidden[1]
         self.activation_output = activation_output[0]
         self.activation_output_derivative = activation_output[1]
         self.output_layer = output_layer
-        self.ideal_values = np.array(ideal_values).reshape(1, -1)
+        self.ideal_values = np.array(ideal_values)
         self.learning_rate = learning_rate
         self.no_of_neurons = [self.input.shape[1]] + hidden_layer + [output_layer]
  
@@ -18,9 +19,9 @@ class NeuralNetwork:
         self.A = [self.input]
         self.Z = []
 
-    def forward_pass(self):
+    def forward_pass(self, input_batch):
         self.Z = []
-        self.A = [self.input]
+        self.A = [input_batch]
 
         for i in range(len(self.W) - 1):
             Z = self.W[i] @ self.A[-1].T + self.b[i].reshape(-1, 1)
@@ -33,20 +34,22 @@ class NeuralNetwork:
         A = self.activation_output(Z)
         self.A.append(A.T)
         
-        return self.loss()
+        return self.loss(input_batch)
 
-    def loss(self):
-        if self.ideal_values.shape[1] != self.output_layer:
+    def loss(self, input_batch):
+        ideal_batch = self.ideal_values[:input_batch.shape[0]]
+        if ideal_batch.shape[1] != self.output_layer:
             raise ValueError("Number of neurons in output layer doesn't match the number of ideal values")
         
-        loss_values = (self.A[-1] - self.ideal_values) ** 2
-        return (0.5)*np.mean(loss_values)
+        loss_values = (self.A[-1] - ideal_batch) ** 2
+        return (0.5) * np.mean(loss_values)
 
-    def backward_pass(self):
-        output_error = self.A[-1] - self.ideal_values #( d(loss)/d(activations))
-        output_delta = output_error * self.activation_output_derivative(self.Z[-1].T) #  (  d(loss)/d(activations) * d(activations)/d(z) ) 
+    def backward_pass(self, input_batch):
+        ideal_batch = self.ideal_values[:input_batch.shape[0]]
+        output_error = self.A[-1] - ideal_batch  # ( d(loss)/d(activations))
+        output_delta = output_error * self.activation_output_derivative(self.Z[-1].T)  #  (  d(loss)/d(activations) * d(activations)/d(z) ) 
         
-        dW = output_delta.T @ self.A[-2] #  d(loss)/d(activations) * d(activations)/d(z) * d(z)/d(w) ---> z = wx + b 
+        dW = output_delta.T @ self.A[-2]  #  d(loss)/d(activations) * d(activations)/d(z) * d(z)/d(w) ---> z = wx + b 
         db = np.sum(output_delta, axis=0)
         
         self.W[-1] -= self.learning_rate * dW  # updating weights (output layer)
@@ -65,11 +68,12 @@ class NeuralNetwork:
     def train(self, epochs):
         loss_values = []
         for _ in range(epochs):
-            loss = self.forward_pass()
+            input_batch = self.input  # Use the entire dataset as the batch
+            loss = self.forward_pass(input_batch)
             loss_values.append(loss)
-            self.backward_pass()
+            self.backward_pass(input_batch)
         return loss_values
-    
+
     def save_model(self, file_path):
         model_params = {
             'W': self.W,
@@ -83,4 +87,4 @@ class NeuralNetwork:
             model_params = pickle.load(f)
         
         self.W = model_params['W']
-        self.b = model_params['b']    
+        self.b = model_params['b']
